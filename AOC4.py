bf03cb7b6c9076db4dc991246505e3bd323b8a43
Stephanie Schwartz <stephmschwartz@gmail.com>
2023-12-04 11:07:30 -0500

    Day 4 -- all in one file for once

---
+++ AOC4.py
@@ -0,0 +1,62 @@
+
+import re
+import functools
+
+
+with open("input4.txt") as f:
+	lines = f.read().splitlines()
+
+def part1(lines):
+    sum = 0
+
+    for line in lines:    
+        parts = re.split(':|\|',line)
+        winning = set(map(int,parts[1].split()))
+        guesses = set(map(int,parts[2].split()))
+        correct = len(winning.intersection(guesses))
+        if correct:
+            sum += 2**(correct-1)
+    return sum
+
+def makeDict(lines):
+    numcopies = {}
+    for line in lines:
+         parts = re.split(':|\|',line)
+         card = int(re.findall('\d+',parts[0])[0])
+         winning = set(map(int,parts[1].split()))
+         guesses = set(map(int,parts[2].split()))
+         correct = len(winning.intersection(guesses)) 
+         numcopies[card] = correct
+    return numcopies
+
+def part2(lines):
+     numcopies = makeDict(lines)
+     totalcards = len(lines)
+     current = numcopies.keys()
+     while len(current):
+        #set up the next set of cards to be checked
+        next = []
+        for card in current:
+            correct = numcopies[card]
+             #if any numbers are winners, need to copy more cards for next time
+            if correct:
+                next.extend(range(card+1,card+correct+1))
+        # count the cards that were added for next round
+        totalcards += len(next)
+        current = next
+     return totalcards
+              
+print("part 1",part1(lines))
+print("part 2",part2(lines))
+
+                
+                      
+
+
+
+
+		
+
+
+
+
