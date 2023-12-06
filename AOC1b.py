9c791e98b530e794ff6356ce12537da6ce25a70c
Stephanie Schwartz <stephmschwartz@gmail.com>
2023-12-06 12:40:56 -0500

    Ugly fix for edge case that my day 1 input didn't have

--- AOC1b.py
+++ AOC1b.py
@@ -15,11 +15,30 @@ num_dict = {
     'nine': 9
 }
 
+rev_num_dict = {
+    'eno': 1,
+    'owt': 2,
+    'eerht': 3,
+    'ruof': 4,
+    'evif': 5,
+    'xis': 6,
+    'neves': 7,
+    'thgie': 8,
+    'enin': 9
+}
 sum = 0
 for line in lines:
 	temp = re.findall(r'\d|one|two|three|four|five|six|seven|eight|nine', line)
-	temp = [int(x) if x.isdigit() else num_dict[x] for x in temp]
-	sum += 10 * temp[0] + temp.pop()
+	temp2 = [int(x) if x.isdigit() else num_dict[x] for x in temp]
+	if not temp[-1].isdigit():
+		revLine = line[::-1]
+		words = re.findall(r"eno|owt|eerht|ruof|evif|xis|neves|thgie|enin",revLine)
+		last = rev_num_dict[words[0]]
+		if not (temp2[-1] == last):
+			temp2[-1] = last
+	num = 10 * temp2[0] + temp2.pop()
+	sum += num
+	print(num)
 		
 		
 
